b983d2c467d28e64705a93873a0d97cc8c27e2ab
Habel <tamikys15@gmail.com>
2022-05-16 20:25:55 +0300

    use inst, and edit Directional Light

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -12,7 +12,7 @@ public class Game : MonoBehaviour
     public GameObject PlayerPrefab => _playerPrefab;
     public GameObject Player => _player;
 
-    //public static Game Instance { get; private set; }
+    public static Game Instance { get; private set; }
 
     public void CreatePlayer(GameObject player)
     {
@@ -24,6 +24,6 @@ public class Game : MonoBehaviour
 
     private void Awake()
     {
-        //Instance = this;
+        Instance = this;
     }
 }
