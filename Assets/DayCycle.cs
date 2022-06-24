cdfbb6bcc256e3f66be0dfcc4213d994c0068841
Habel <tamikys15@gmail.com>
2022-06-24 16:00:55 +0300

    hw 3: 1,2,3 complete; and sun cicle script

--- Assets/DayCycle.cs
+++ Assets/DayCycle.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class DayCycle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void LateUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.rotation = Quaternion.Euler(.2f, 0, 0) * transform.rotation;
     }
 }
